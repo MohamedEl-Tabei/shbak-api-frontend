d631802e317fa068ebd6a1fa3afc9b978ab931a4
unknown <mohamedeltab33y@gmail.com>
2024-09-30 17:18:36 +0300

    login

--- src/pages/login.js
+++ src/pages/login.js
@@ -9,8 +9,8 @@ const Login = () => {
   let l = Data.RegisterLabels;
   let labels = [l[0], l[1]];
   let [err, setErr] = useState("Error");
-  let [email, setEmail] = useState("mohamedeltab33y@gmail.com");
-  let [password, setPassword] = useState("Sh12345**");
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
   let [rememberMe, setRememberMe] = useState(false);
   let [user, setUser] = useState({ fName: undefined });
   let [loading, setLoading] = useState(false);
